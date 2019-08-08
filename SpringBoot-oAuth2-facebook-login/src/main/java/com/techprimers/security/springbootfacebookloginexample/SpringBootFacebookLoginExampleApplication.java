8ad874288fb8adb295a60f0ab954141175768469
satya <satya.iton@gmail.com>
2019-08-08 10:00:08 +0530

    SpringBoot-oAuth2-facebook-login

---
+++ SpringBoot-oAuth2-facebook-login/src/main/java/com/techprimers/security/springbootfacebookloginexample/SpringBootFacebookLoginExampleApplication.java
@@ -0,0 +1,14 @@
+package com.techprimers.security.springbootfacebookloginexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootFacebookLoginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFacebookLoginExampleApplication.class, args);
+	}
+
+}
